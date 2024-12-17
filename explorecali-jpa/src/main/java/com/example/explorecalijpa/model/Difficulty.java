a7eea46305dcb580f64b72a4e6faec8a13098b81
KentDev144 <kent1441212@gmail.com>
2024-12-17 16:07:18 +0000

    fix

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/model/Difficulty.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/model/Difficulty.java
@@ -7,7 +7,7 @@ package com.example.explorecalijpa.model;
 /**
  * Enumeration of the level of effort.
  *
- * Created by Mary Ellen Bowman
+ * Created by Mary 
  */
 public enum Difficulty {
     Easy, Medium, Difficult, Varies;
