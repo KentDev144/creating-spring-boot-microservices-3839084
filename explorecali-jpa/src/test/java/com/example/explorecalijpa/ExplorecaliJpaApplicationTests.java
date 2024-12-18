a37495134a9128beff4bcf50718798c3572dcd54
KentDev144 <kent1441212@gmail.com>
2024-12-18 09:13:46 +0000

    fix

--- explorecali-jpa/src/test/java/com/example/explorecalijpa/ExplorecaliJpaApplicationTests.java
+++ explorecali-jpa/src/test/java/com/example/explorecalijpa/ExplorecaliJpaApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.explorecalijpa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ExplorecaliJpaApplicationTests {
 
 	@Test
